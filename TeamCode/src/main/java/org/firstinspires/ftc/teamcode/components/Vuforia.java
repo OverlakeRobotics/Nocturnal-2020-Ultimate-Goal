e50feb39b99415d12fa481256ea50325a602a00f
goodnessidontknow <49700385+goodnessidontknow@users.noreply.github.com>
2020-09-30 16:45:53 -0700

    Added documentation, Ultimate Goal assets for Vuforia, including OpenGLMatrix values, and readded perimeter targets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Vuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Vuforia.java
@@ -35,22 +35,19 @@ public class Vuforia {
     private static final String VUFORIA_KEY =
             "Ad0Srbr/////AAABmdpa0/j2K0DPhXQjE2Hyum9QUQXZO8uAVCNpwlogfxiVmEaSuqHoTMWcV9nLlQpEnh5bwTlQG+T35Vir8IpdrSdk7TctIqH3QBuJFdHsx5hlcn74xa7AiQSJgUD/n7JJ2zJ/Er5Hc+b+r616Jf1YU6RO63Ajk5+TFB9N3a85NjMD6eDm+C6f14647ELnmGC03poSOeczbX7hZpIEObtYdVyKZ2NQ/26xDfSwwJuyMgUHwWY6nl6mk0GMnIGvu0/HoGNgyR5EkUQWyx9XlmxSrldY7BIEVkiKmracvD7W9hEGZ2nPied6DTY5RFNuFX07io6+I59/d7291NXKVMDnFAqSt4a2JYsECv+j7b25S0mD";
 
-    private static final float mmPerInch        = 25.4f;
+    private static final float mmPerInch        = 25.4f;                    // constant for converting measurements from inches to millimeters
     private static final float mmTargetHeight   = (6) * mmPerInch;          // the height of the center of the target image above the floor
-    private static final float stoneZ = 2.00f * mmPerInch;
-    private static final float bridgeZ = 6.42f * mmPerInch;
-    private static final float bridgeY = 23 * mmPerInch;
-    private static final float bridgeX = 5.18f * mmPerInch;
-    private static final float bridgeRotY = 59;                                 // Units are degrees
-    private static final float bridgeRotZ = 180;
-    private static final float halfField = 72 * mmPerInch;
+
+    private static final float halfField = 72 * mmPerInch;                  // constants for perimeter targets
     private static final float quadField  = 36 * mmPerInch;
-    private OpenGLMatrix lastLocation = null;
+
+    private OpenGLMatrix lastLocation = null; // class members
     private VuforiaLocalizer vuforia = null;
     private float phoneXRotate    = 0;
     private float phoneYRotate    = 0;
     private float phoneZRotate    = 0;
-    public VuforiaTrackables targetsSkyStone;
+    public VuforiaTrackables targetsUltGoal;
+    public VuforiaTrackables targetsField;
     private List<VuforiaTrackable> allTrackables;
 
     public Vuforia(HardwareMap hardwareMap, CameraChoice choice) {
@@ -98,11 +95,13 @@ public class Vuforia {
     }
 
     public void activate() {
-        targetsSkyStone.activate();
+        targetsUltGoal.activate();
+        targetsField.activate();
     }
 
     public void disable() {
-        targetsSkyStone.deactivate();
+        targetsUltGoal.deactivate();
+        targetsField.deactivate();
     }
 
     public VuforiaLocalizer setCamera(HardwareMap hardwareMap, CameraChoice cameraChoice) {
@@ -134,62 +133,69 @@ public class Vuforia {
     }
 
     private void initializeTrackables(VuforiaLocalizer vuforia) {
-        targetsSkyStone = vuforia.loadTrackablesFromAsset("Skystone");
+        targetsUltGoal = vuforia.loadTrackablesFromAsset("UltimateGoal");
+        targetsField = vuforia.loadTrackablesFromAsset("Skystone");
 
-        VuforiaTrackable stoneTarget = targetsSkyStone.get(0);
-        stoneTarget.setName("Stone Target");
-        VuforiaTrackable blueRearBridge = targetsSkyStone.get(1);
-        blueRearBridge.setName("Blue Rear Bridge");
-        VuforiaTrackable redRearBridge = targetsSkyStone.get(2);
-        redRearBridge.setName("Red Rear Bridge");
-        VuforiaTrackable redFrontBridge = targetsSkyStone.get(3);
-        redFrontBridge.setName("Red Front Bridge");
-        VuforiaTrackable blueFrontBridge = targetsSkyStone.get(4);
-        blueFrontBridge.setName("Blue Front Bridge");
-        VuforiaTrackable red1 = targetsSkyStone.get(5);
+        VuforiaTrackable blueTowerGoalTarget = targetsUltGoal.get(0);
+        blueTowerGoalTarget.setName("Blue Tower Goal");
+        VuforiaTrackable redTowerGoalTarget = targetsUltGoal.get(1);
+        redTowerGoalTarget.setName("Red Tower Goal");
+        VuforiaTrackable redAllianceTarget = targetsUltGoal.get(2);
+        redAllianceTarget.setName("Red Alliance");
+        VuforiaTrackable blueAllianceTarget = targetsUltGoal.get(3);
+        blueAllianceTarget.setName("Blue Alliance");
+        VuforiaTrackable frontWallTarget = targetsUltGoal.get(4);
+        frontWallTarget.setName("Front Wall");
+        VuforiaTrackable red1 = targetsField.get(5);
         red1.setName("Red Perimeter 1");
-        VuforiaTrackable red2 = targetsSkyStone.get(6);
+        VuforiaTrackable red2 = targetsField.get(6);
         red2.setName("Red Perimeter 2");
-        VuforiaTrackable front1 = targetsSkyStone.get(7);
+        VuforiaTrackable front1 = targetsField.get(7);
         front1.setName("Front Perimeter 1");
-        VuforiaTrackable front2 = targetsSkyStone.get(8);
+        VuforiaTrackable front2 = targetsField.get(8);
         front2.setName("Front Perimeter 2");
-        VuforiaTrackable blue1 = targetsSkyStone.get(9);
+        VuforiaTrackable blue1 = targetsField.get(9);
         blue1.setName("Blue Perimeter 1");
-        VuforiaTrackable blue2 = targetsSkyStone.get(10);
+        VuforiaTrackable blue2 = targetsField.get(10);
         blue2.setName("Blue Perimeter 2");
-        VuforiaTrackable rear1 = targetsSkyStone.get(11);
+        VuforiaTrackable rear1 = targetsField.get(11);
         rear1.setName("Rear Perimeter 1");
-        VuforiaTrackable rear2 = targetsSkyStone.get(12);
+        VuforiaTrackable rear2 = targetsField.get(12);
         rear2.setName("Rear Perimeter 2");
 
         // For convenience, gather together all the trackable objects in one easily-iterable collection */
         allTrackables = new ArrayList<VuforiaTrackable>();
-        allTrackables.addAll(targetsSkyStone);
+        allTrackables.addAll(targetsUltGoal);
 
         // Set the position of the Stone Target.  Since it's not fixed in position, assume it's at the field origin.
         // Rotated it to to face forward, and raised it to sit on the ground correctly.
         // This can be used for generic target-centric approach algorithms
-        stoneTarget.setLocation(OpenGLMatrix
-                .translation(0, 0, stoneZ)
+
+        //TODO: Update these things.
+
+        /*
+        target.setLocation(OpenGLMatrix
+                .translation(X position, Y position, Z position) // Actual location on the field with coordinates
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, X rotation, Y rotation, Z rotation))); // Rotation
+        */
+        redAllianceTarget.setLocation(OpenGLMatrix
+                .translation(0, -halfField, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180)));
+
+        blueAllianceTarget.setLocation(OpenGLMatrix
+                .translation(0, halfField, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0)));
+        frontWallTarget.setLocation(OpenGLMatrix
+                .translation(-halfField, 0, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , 90)));
+
+        // The tower goal targets are located a quarter field length from the ends of the back perimeter wall.
+        blueTowerGoalTarget.setLocation(OpenGLMatrix
+                .translation(halfField, quadField, mmTargetHeight)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , -90)));
+        redTowerGoalTarget.setLocation(OpenGLMatrix
+                .translation(halfField, -quadField, mmTargetHeight)
                 .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
-
-        //Set the position of the bridge support targets with relation to origin (center of field)
-        blueFrontBridge.setLocation(OpenGLMatrix
-                .translation(-bridgeX, bridgeY, bridgeZ)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0, bridgeRotY, bridgeRotZ)));
-
-        blueRearBridge.setLocation(OpenGLMatrix
-                .translation(-bridgeX, bridgeY, bridgeZ)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0, -bridgeRotY, bridgeRotZ)));
-
-        redFrontBridge.setLocation(OpenGLMatrix
-                .translation(-bridgeX, -bridgeY, bridgeZ)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0, -bridgeRotY, 0)));
-
-        redRearBridge.setLocation(OpenGLMatrix
-                .translation(bridgeX, -bridgeY, bridgeZ)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 0, bridgeRotY, 0)));
 
         //Set the position of the perimeter targets with relation to origin (center of field)
         red1.setLocation(OpenGLMatrix
@@ -238,6 +244,7 @@ public class Vuforia {
 //            ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(robotFromCamera, parameters.cameraDirection);
 //        }
 
-        targetsSkyStone.activate();
+        targetsUltGoal.activate();
+        targetsField.activate();
     }
 }

f595f7d25e4fa3f571941d428c631ed695051f7f
Adrian Avram <Adrian_Avram@Outlook.com>
2020-09-30 16:15:38 -0700

    Add files from old repo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/DriveTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.opmodes.autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.components.RoadRunnerDriveSystem;
+
+public class DriveTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RoadRunnerDriveSystem drive = new RoadRunnerDriveSystem(hardwareMap);
+
+        Trajectory trajectory = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(30)
+                .forward(10)
+                .splineToConstantHeading()
+                .build();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        drive.turn(Math.toRadians(90));
+
+        drive.followTrajectory(trajectory);
+    }
+}

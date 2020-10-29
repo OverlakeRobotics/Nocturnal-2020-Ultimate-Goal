724fa593fc24d749a7e60bb59d5a85a2c53f0e79
anishch <43155397+anishch@users.noreply.github.com>
2020-10-28 17:04:52 -0700

    TensorFlow class can get targetRegion now, there are things in the runOpMode method that can be used later for finding things like height and pushing objects over specifically, but I'm not going to focus on it right now.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Tensorflow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Tensorflow.java
@@ -10,18 +10,19 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
 public class Tensorflow {
 
     public enum SquareState{
-        BOX_A, BOX_B, BOX_C
+        BOX_A, BOX_B, BOX_C,
     }
 
     EnumMap<SquareState, Integer> boxCoordinates;
 
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
-    private static final String LABEL_FIRST_ELEMENT = "Zero"; //ZeroRings
+    private static final String LABEL_FIRST_ELEMENT = "Four"; //FourRings
     private static final String LABEL_SECOND_ELEMENT = "One"; //OneRing
-    private static final String LABEL_THIRD_ELEMENT = "Four"; //FourRing
     private static final String VUFORIA_KEY = "Ad0Srbr/////AAABmdpa0/j2K0DPhXQjE2Hyum9QUQXZO8uAVCNpwlogfxiVmEaSuqHoTMWcV9nLlQpEnh5bwTlQG+T35Vir8IpdrSdk7TctIqH3QBuJFdHsx5hlcn74xa7AiQSJgUD/n7JJ2zJ/Er5Hc+b+r616Jf1YU6RO63Ajk5+TFB9N3a85NjMD6eDm+C6f14647ELnmGC03poSOeczbX7hZpIEObtYdVyKZ2NQ/26xDfSwwJuyMgUHwWY6nl6mk0GMnIGvu0/HoGNgyR5EkUQWyx9XlmxSrldY7BIEVkiKmracvD7W9hEGZ2nPied6DTY5RFNuFX07io6+I59/d7291NXKVMDnFAqSt4a2JYsECv+j7b25S0mD";;
 
     private VuforiaLocalizer vuforia; //declaring VuforiaLocalizer - converts Vuforia Frame into AndroidBitMap
@@ -39,7 +40,7 @@ public class Tensorflow {
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorId); //creating parameters
         tfodParameters.minResultConfidence = 0.3f; //minimumConfidenceNecessaryForActingOnDetection
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia); //create objectDetector
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT, LABEL_THIRD_ELEMENT); //loading models
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT); //loading models
         tfod.activate(); //turnOn
         //Hi, this is Anish, and I'm teaching Vincent and Ethan, how git works.
     }
@@ -65,8 +66,36 @@ public class Tensorflow {
     } //deactivate
 
     public Enum getTargetRegion(){
-
-        ArrayList<Float> heights = new ArrayList<Float>();
+        List<Recognition> toOperateOffOf = getInference();
+        if (toOperateOffOf.size() == 1){
+            int i = 0;
+            for (Recognition recognitions : toOperateOffOf){
+                telemetry.addData(String.format("label (%d)", i), recognitions.getLabel());
+                telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                        recognitions.getLeft(), recognitions.getTop());
+                telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                        recognitions.getRight(), recognitions.getBottom());
+                if (recognitions.getConfidence() >= 0.4){
+                    if (recognitions.getLabel().equals("Four")){
+                        telemetry.addData("Returning SquareState", SquareState.BOX_A);
+                        return SquareState.BOX_A;
+                    }
+                    else{
+                        telemetry.addData("Returning SquareState", SquareState.BOX_B);
+                        return SquareState.BOX_B;
+                    }
+                }
+                telemetry.addData("Returning SquareState", SquareState.BOX_C);
+                return SquareState.BOX_C;
+            }
+        }
+        else{
+            telemetry.addData("Returning SquareState", SquareState.BOX_C);
+            return SquareState.BOX_C;
+        }
+        telemetry.addData("Oopsie", null);
+        return null;
+        /*ArrayList<Float> heights = new ArrayList<Float>();
         int length = getInference().size();
         for (Recognition recognitions : getInference()){
             if (recognitions.getConfidence() >= 0.4){
@@ -101,7 +130,7 @@ public class Tensorflow {
                 return SquareState.BOX_C;
             }
         }
-        return null;
+        return null;*/
     }
 
 }

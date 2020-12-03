3f524e501eee9c8ac7e9d1ff730a676d9fbfe6fb
anishch <43155397+anishch@users.noreply.github.com>
2020-12-02 16:30:22 -0800

    made some more problems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Tensorflow.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/Tensorflow.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.components;
 
-import com.qualcomm.hardware.BuildConfig;
-
 import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
@@ -11,6 +9,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.teamcode.BuildConfig;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
@@ -25,7 +24,7 @@ public class Tensorflow {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Four"; //FourRings
     private static final String LABEL_SECOND_ELEMENT = "One"; //OneRing
-    private static final String VUFORIA_KEY = BuildConfig.VUFORIA_KEY;
+    private static final String VUFORIA_KEY = BuildConfig.NOCTURNAL_VUFORIA_KEY;
             //"Ad0Srbr/////AAABmdpa0/j2K0DPhXQjE2Hyum9QUQXZO8uAVCNpwlogfxiVmEaSuqHoTMWcV9nLlQpEnh5bwTlQG+T35Vir8IpdrSdk7TctIqH3QBuJFdHsx5hlcn74xa7AiQSJgUD/n7JJ2zJ/Er5Hc+b+r616Jf1YU6RO63Ajk5+TFB9N3a85NjMD6eDm+C6f14647ELnmGC03poSOeczbX7hZpIEObtYdVyKZ2NQ/26xDfSwwJuyMgUHwWY6nl6mk0GMnIGvu0/HoGNgyR5EkUQWyx9XlmxSrldY7BIEVkiKmracvD7W9hEGZ2nPied6DTY5RFNuFX07io6+I59/d7291NXKVMDnFAqSt4a2JYsECv+j7b25S0mD";;
 
     private VuforiaLocalizer vuforia; //declaring VuforiaLocalizer - converts Vuforia Frame into AndroidBitMap

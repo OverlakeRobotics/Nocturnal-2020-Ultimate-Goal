5c4b66f4eeb637e6e080b26d8e8d012942cd4fa8
anishch <43155397+anishch@users.noreply.github.com>
2020-12-14 21:33:57 -0800

    some random changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/ArmSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/ArmSystem.java
@@ -1,4 +1,43 @@
 package org.firstinspires.ftc.teamcode.components;
 
-class ArmSystem {
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ArmSystem {
+
+    Servo rotator;
+    DcMotor outwardextender;
+    DcMotor lowerraise; //or should this be a Servo?
+
+    private static final double RETRACTED = 6.8; //wrong, incorrect placeholder values that need to be updated
+    private static final double EXTENDED = 13.8; //^^
+
+    private static final double RAISED = 3.4; // ^^
+    private static final double LOWERED = 2.8; // ^^
+
+    private static final double ZERO = 0.0; // ^^^
+
+
+    public ArmSystem(Servo servo, DcMotor motor1, DcMotor motor2) { //constructor
+        this.rotator = servo;
+        this.outwardextender = motor1;
+        this.lowerraise = motor2;
+    }
+
+    private void init() {
+
+        while (outwardextender.getCurrentPosition() != RETRACTED) {
+            outwardextender.setPower(-1);
+        }
+
+        while (lowerraise.getCurrentPosition() != RAISED) {
+            lowerraise.setPower(-1);
+        }
+        rotator.setPosition(ZERO);
+    }
+}
+
+
+
+
 }

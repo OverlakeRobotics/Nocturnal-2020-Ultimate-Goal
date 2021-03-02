3032e6ea5d58cd44ee44503b8316e05f95281756
vpritamani <44382864+vpritamani@users.noreply.github.com>
2021-03-01 16:36:05 -0800

    Created JUnitTests.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/JUnitTests.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import org.firstinspires.ftc.teamcode.components.Encoder;
+import org.firstinspires.ftc.teamcode.components.IMUSystem;
+
+import static org.firstinspires.ftc.teamcode.helpers.Constants.encoderTicksToInches;
+
+class JUnitTests extends AutonomousSelfCheck{
+    private Encoder testEncoder;
+    private IMUSystem testIMUSystem;
+
+    @Override
+    public void init(){
+        
+    }
+}

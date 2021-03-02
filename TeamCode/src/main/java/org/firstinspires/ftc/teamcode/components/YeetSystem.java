0933ee33a1226ec2703c2a088255d15c0d8f1321
SansDoge <alan.yyd@gmail.com>
2021-03-01 16:47:27 -0800

    Major changes to YeetSystem.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/YeetSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/YeetSystem.java
@@ -5,50 +5,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.helpers.Constants;
 
-import java.util.EnumMap;
-
-import static org.firstinspires.ftc.teamcode.helpers.Constants.LEFT_CLOSED_POSITION;
-import static org.firstinspires.ftc.teamcode.helpers.Constants.LEFT_OPEN_POSITION;
-import static org.firstinspires.ftc.teamcode.helpers.Constants.RIGHT_CLOSED_POSITION;
-import static org.firstinspires.ftc.teamcode.helpers.Constants.RIGHT_OPEN_POSITION;
-
 public class YeetSystem {
 
-    //TODO 2 Servos, arm to bottom right or robot, hardcode max and min angles for motor
-
-    public enum Servos {
-        LEFT (LEFT_CLOSED_POSITION, LEFT_OPEN_POSITION), //values need to be changed
-        RIGHT (RIGHT_CLOSED_POSITION, RIGHT_OPEN_POSITION); //values need to be changed
-
-        private final double openPosition;
-        private final double closedPosition;
-
-        Servos(double closedPosition, double openPosition) {
-            this.closedPosition = closedPosition;
-            this.openPosition = openPosition;
-        }
-
-        public double getOpenPosition() {
-            return openPosition;
-        }
-
-        public double getClosedPosition() {
-            return closedPosition;
-        }
-    }
-
-    public EnumMap<Servos, Servo> servoMap;
-
+    // Systems
     private final DcMotor motor; //one motor that we need
+    private Servo leftServo;
+    private Servo rightServo;
 
-    private static final double TICKS_PER_REVOLUTION = Constants.TICKS_PER_REV; //number of ticks per revolution
-    private static final double NUM_REVOLUTIONS = 0.8; // THIS NEEDS TO BE CHANGED - the number is num of revolutions
-    private static final double UP_POSITION = TICKS_PER_REVOLUTION * NUM_REVOLUTIONS;
-
-    private static final int DEFAULT = 0; // this needs to be changed
-
-    public YeetSystem(DcMotor motor) { //constructor
+    public YeetSystem(DcMotor motor, Servo leftServo, Servo rightServo) { //constructor
         this.motor = motor; //setting ArmSystem motor to whatever motor that is
+        this.leftServo = leftServo;
+        this.rightServo = rightServo;
         grab();
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
@@ -83,8 +50,8 @@ public class YeetSystem {
      */
     private void armUp() {
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        while (motor.getCurrentPosition() != UP_POSITION){
-            motor.setPower(0.75);
+        while (motor.getCurrentPosition() != Constants.ARM_MOTOR_UP_POSITION) {
+            motor.setPower(Constants.ARM_MOTOR_RAW_POWER);
         }
         motor.setPower(0);
     }
@@ -94,8 +61,8 @@ public class YeetSystem {
      */
     private void armDown() {
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        while (motor.getCurrentPosition() != DEFAULT) {
-            motor.setTargetPosition(DEFAULT);
+        while (motor.getCurrentPosition() != Constants.ARM_MOTOR_DOWN_POSITION) {
+            motor.setPower(-Constants.ARM_MOTOR_RAW_POWER);
         }
         motor.setPower(0.0);
     }
@@ -104,17 +71,15 @@ public class YeetSystem {
      * Closes the servos to grab the wobble goal
      */
     private void grab() {
-        servoMap.forEach((name, servo) -> servo.setPosition(name.getClosedPosition()));
+        leftServo.setPosition(Constants.LEFT_ARM_SERVO_CLOSED_POSITION);
+        rightServo.setPosition(Constants.RIGHT_ARM_SERVO_CLOSED_POSITION);
     }
 
     /**
      * Opens the servos to release the wobble goal
      */
     private void release() {
-        servoMap.forEach((name, servo) -> servo.setPosition(name.getOpenPosition()));
+        leftServo.setPosition(Constants.LEFT_ARM_SERVO_OPEN_POSITION);
+        rightServo.setPosition(Constants.RIGHT_ARM_SERVO_OPEN_POSITION);
     }
 }
-
-
-
-

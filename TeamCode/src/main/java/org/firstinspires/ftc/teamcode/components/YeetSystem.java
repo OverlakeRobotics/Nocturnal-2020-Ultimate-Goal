6839bad0e7e147fb2132456edafca30ea338bd32
SansDoge <alan.yyd@gmail.com>
2021-03-09 16:47:18 -0800

    Added wait to YeetSystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/YeetSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/YeetSystem.java
@@ -5,16 +5,17 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.helpers.Constants;
 
+import java.util.concurrent.TimeUnit;
+
 public class YeetSystem {
 
     // Systems
     private final DcMotor motor; //one motor that we need
-    private Servo leftServo;
-    private Servo rightServo;
+    private final Servo leftServo;
+    private final Servo rightServo;
 
     // Tracker fields
-    private double targetPosition;
-    private boolean isRunning;
+    private int targetPosition;
 
     public YeetSystem(DcMotor motor, Servo leftServo, Servo rightServo) { //constructor
         this.motor = motor; //setting ArmSystem motor to whatever motor that is
@@ -29,12 +30,12 @@ public class YeetSystem {
     public boolean placed() {
         if (isComplete()) {
             release();
-            powerDown();
-        } else if (!isRunning) {
+            return true;
+        } else if (!motor.isBusy()) {
             targetPosition = Constants.ARM_MOTOR_DOWN_POSITION;
             moveArm();
         }
-        return isComplete();
+        return false;
     }
 
     /**
@@ -43,14 +44,14 @@ public class YeetSystem {
     public boolean pickUp() {
         if (isComplete()) {
             powerDown();
-        } else if (!isRunning) {
+            return true;
+        } else if (!motor.isBusy()) {
             targetPosition = Constants.ARM_MOTOR_UP_POSITION;
-
-            //TODO test if the grab finishes before moveArm() is called
             grab();
+            massTimesGravity();
             moveArm();
         }
-        return isComplete();
+        return false;
     }
 
     /**
@@ -66,6 +67,17 @@ public class YeetSystem {
     }
 
     /**
+     * Waits a certain amount of time in milliseconds
+     */
+    private void massTimesGravity() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(Constants.SERVO_WAIT_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
      * Checks if the system still needs to run
      * @return if the system still needs to run
      */
@@ -78,7 +90,6 @@ public class YeetSystem {
      */
     private void powerDown() {
         motor.setPower(0.0);
-        isRunning = false;
     }
 
     /**
@@ -86,12 +97,14 @@ public class YeetSystem {
      */
     private void moveArm() {
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setTargetPosition(targetPosition);
         if (targetPosition == Constants.ARM_MOTOR_DOWN_POSITION) {
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             motor.setPower(-Constants.ARM_MOTOR_RAW_POWER);
         } else {
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
             motor.setPower(Constants.ARM_MOTOR_RAW_POWER);
         }
-        isRunning = true;
     }
 
     /**
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/helpers/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 
     private final static int CPS_STEP = 0x10000;
 
-    public static double TICKS_PER_REV = 2400;
+    public static int TICKS_PER_REV = 2400;
     public static double WHEEL_RADIUS = 1; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
@@ -104,11 +104,12 @@ public class Constants {
     //YeetSystem
     //TODO Change these numbers
     public static final double ARM_MOTOR_NUM_REVOLUTIONS = 0.8; // THIS NEEDS TO BE CHANGED - the number is num of revolutions
-    public static final double ARM_MOTOR_UP_POSITION = Constants.TICKS_PER_REV * ARM_MOTOR_NUM_REVOLUTIONS;
+    public static final int ARM_MOTOR_UP_POSITION = (int)(Constants.TICKS_PER_REV * ARM_MOTOR_NUM_REVOLUTIONS);
     public static final int ARM_MOTOR_DOWN_POSITION = 0; // this needs to be changed
     public static final double ARM_MOTOR_RAW_POWER = 0.75;
     public static final double LEFT_ARM_SERVO_CLOSED_POSITION = 0.0;
     public static final double RIGHT_ARM_SERVO_CLOSED_POSITION = 0.48;
     public static final double LEFT_ARM_SERVO_OPEN_POSITION = 0.4;
     public static final double RIGHT_ARM_SERVO_OPEN_POSITION = 0.1;
+    public static final int SERVO_WAIT_TIME = 500;
 }

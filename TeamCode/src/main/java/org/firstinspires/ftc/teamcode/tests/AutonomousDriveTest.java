fb870b1a370fa986ee16b1d025235a59f8ca4cea
Adrian Avram <Adrian_Avram@Outlook.com>
2021-03-15 16:27:06 -0700

    Updated constants, fixed shootersystem, updated target power

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/AutonomousDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/AutonomousDriveTest.java
@@ -4,100 +4,106 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.teamcode.components.RoadRunnerDriveSystem;
+import org.firstinspires.ftc.teamcode.components.ShootingSystem;
 import org.firstinspires.ftc.teamcode.components.Tensorflow;
 import org.firstinspires.ftc.teamcode.components.VuforiaSystem;
+import org.firstinspires.ftc.teamcode.components.YeetSystem;
 import org.firstinspires.ftc.teamcode.helpers.Constants;
 import org.firstinspires.ftc.teamcode.helpers.Coordinates;
 import org.firstinspires.ftc.teamcode.helpers.GameState;
+import org.firstinspires.ftc.teamcode.helpers.Target;
 
 @Autonomous(name = "AutonomousDriveTest", group = "Autonomous")
 public class AutonomousDriveTest extends OpMode {
 
     // Variables
     protected GameState currentGameState;
-    public static Tensorflow.SquareState targetRegion;
-    protected boolean deliveredFirstWobble;
-
-    // Systems
-    protected Tensorflow tensorflow;
 
     // Variables
-    private static final float mmPerInch = 25.4f;
     protected Pose2d currentPosition;
 
     // Systems
-    protected RoadRunnerDriveSystem roadRunnerDriveSystem;
-    protected VuforiaSystem vuforia;
+//    protected RoadRunnerDriveSystem roadRunnerDriveSystem;
+//    protected YeetSystem yeetSystem;
+    ShootingSystem shootingSystem;
+    protected ElapsedTime elapsedTime;
 
     @Override
     public void init() {
         this.msStuckDetectInit = 20000;
         this.msStuckDetectInitLoop = 20000;
+        elapsedTime = new ElapsedTime();
 
         currentPosition = new Pose2d(Coordinates.STARTING_POSITION.getX(), Coordinates.STARTING_POSITION.getY(), Math.PI);
-        vuforia = VuforiaSystem.getInstance();
 
         //Initialize RoadRunner
         try {
-            roadRunnerDriveSystem = new RoadRunnerDriveSystem(hardwareMap);
-            roadRunnerDriveSystem.setPoseEstimate(currentPosition);
+//            roadRunnerDriveSystem = new RoadRunnerDriveSystem(hardwareMap);
+//            roadRunnerDriveSystem.setPoseEstimate(currentPosition);
         } catch (Exception e) {
             telemetry.addData(Constants.ROBOT_SYSTEM_ERROR, e.getStackTrace());
         }
 
-        tensorflow = new Tensorflow(hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName()));
-        tensorflow.activate();
+        shootingSystem = new ShootingSystem(hardwareMap.get(DcMotor.class, "ShootingSystem"), hardwareMap.get(Servo.class, "ShootingSystemServo"));
+
+//        yeetSystem = new YeetSystem(hardwareMap.get(DcMotor.class, "YeetSystem"), hardwareMap.get(Servo.class, "LeftArmServo"), hardwareMap.get(Servo.class, "RightArmServo"));
         newGameState(GameState.INITIAL);
     }
 
     @Override
     public void init_loop() {
-        targetRegion = tensorflow.getTargetRegion();
+
     }
 
     @Override
     public void start() {
-        tensorflow.shutdown();
         super.start();
     }
 
     @Override
     public void loop() {
-        vuforiaData();
         telemetry.addData("GameState", currentGameState);
 
 
         // Makes sure the trajectory is finished before doing anything else
-        boolean trajectoryFinished = roadRunnerDriveSystem.update();
-        Pose2d poseEstimate = roadRunnerDriveSystem.getPoseEstimate();
-        telemetry.addData("x", poseEstimate.getX());
-        telemetry.addData("y", poseEstimate.getY());
-        telemetry.addData("heading", poseEstimate.getHeading());
+//        boolean trajectoryFinished = roadRunnerDriveSystem.update();
+//        Pose2d poseEstimate = roadRunnerDriveSystem.getPoseEstimate();
+//        telemetry.addData("x", poseEstimate.getX());
+//        telemetry.addData("y", poseEstimate.getY());
+//        telemetry.addData("heading", poseEstimate.getHeading());
         telemetry.update();
 
         switch (currentGameState) {
             case INITIAL:
                 // Initialize
                 newGameState(GameState.DELIVER_WOBBLE);
+                elapsedTime.reset();
                 break;
 
             case DELIVER_WOBBLE:
-                if (trajectoryFinished) {
+                shootingSystem.warmUp(Target.POWER_SHOT);
+                if (elapsedTime.seconds() > 2) {
+                    elapsedTime.reset();
                     newGameState(GameState.CALIBRATE_LOCATION);
                 }
+
                 break;
 
             case CALIBRATE_LOCATION:
-                if (trajectoryFinished) {
+                shootingSystem.warmUp(Target.POWER_SHOT);
+                if (shootingSystem.shoot()) {
                     newGameState(GameState.COMPLETE);
                 }
                 break;
 
             case DRIVE_TO_SHOOTING_LOCATION:
-                //TODO drive to shooting location, start up shooter motor
-                newGameState(GameState.POWERSHOT);
+
                 break;
 
             case POWERSHOT:
@@ -129,13 +135,7 @@ public class AutonomousDriveTest extends OpMode {
 
     @Override
     public void stop() {
-        if (vuforia != null) {
-            vuforia.disable();
-        }
 
-        if (tensorflow != null) {
-            tensorflow.shutdown();
-        }
     }
 
     /**
@@ -144,39 +144,5 @@ public class AutonomousDriveTest extends OpMode {
      */
     protected void newGameState(GameState newGameState) {
         currentGameState = newGameState;
-        currentPosition = roadRunnerDriveSystem.getPositionEstimate();
-        Trajectory trajectory = TestTrajectories.getTrajectory(currentGameState, currentPosition);
-        if (trajectory != null) {
-            roadRunnerDriveSystem.followTrajectoryAsync(trajectory);
-        }
-    }
-
-    /**
-     * Calibrates RoadRunner using Vuforia data
-     * Because camera is sideways, the x offset corresponds to y coordinates and visa versa
-     * Vuforia is in millimeters and everything else is in inches
-     */
-    private void calibrateLocation() {
-        double xUpdate = Coordinates.CALIBRATION.getX() - (vuforia.getYOffset() / Constants.mmPerInch - Constants.fieldBoxWidth);
-        double yUpdate = Coordinates.CALIBRATION.getY() + vuforia.getXOffset() / Constants.mmPerInch;
-        roadRunnerDriveSystem.setPoseEstimate(new Pose2d(xUpdate, yUpdate));
-    }
-
-    protected void vuforiaData() {
-        VectorF translation = vuforia.vector();
-
-        // only one of these two will be used
-        if (translation != null) {
-            telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                    translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
-        }
-        telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                vuforia.getXOffset() / mmPerInch, vuforia.getYOffset() / mmPerInch, vuforia.getZOffset() / mmPerInch);
-
-        if (translation != null) {
-            telemetry.addLine("null");
-        } else {
-            telemetry.addLine("not null");
-        }
     }
 }

cb7b6f971247c1650700180417ebc25dc04f9444
Brandon <bmarcus2003@gmail.com>
2021-03-22 16:29:30 -0700

    fixed yeetSystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/AutonomousSelfCheck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/AutonomousSelfCheck.java
@@ -26,17 +26,13 @@ public class AutonomousSelfCheck extends AutonomousOpMode {
         switch (currentGameState) { // TODO: This monstrosity.
             //TODO Do we need a trajectory as a field?
             case TEST_YEET_UP:
-//                yeetSystem.pickup();
-//                if (yeetSystem.isUp()) {
-//                    yeetSystem.powerDown();
-//                    newGameState(GameState.TEST_YEET_DOWN);
-//                }
+                if (yeetSystem.pickedUp()) {
+                    newGameState(GameState.TEST_YEET_DOWN);
+                }
             case TEST_YEET_DOWN:
-                yeetSystem.placed();
-//                if (yeetSystem.isDown()) {
-//                    yeetSystem.powerDown();
-//                    newGameState(GameState.TEST_SHOOTING);
-//                }
+                if (yeetSystem.placed()) {
+                    newGameState(GameState.TEST_SHOOTING);
+                }
             case TEST_SHOOTING:
                 shootingSystem.warmUp(Target.POWER_SHOT);
                 shootingSystem.shoot();
